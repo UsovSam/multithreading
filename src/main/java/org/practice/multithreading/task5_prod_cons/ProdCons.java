20fc9f5576e5ba0cb7b6dfc8d254fbe798f576a3
Sam <usov.semen@gmail.com>
2025-04-22 20:26:27 +0300

    updated

--- src/main/java/org/practice/multithreading/task5_prod_cons/ProdCons.java
+++ src/main/java/org/practice/multithreading/task5_prod_cons/ProdCons.java
@@ -1,21 +1,22 @@
-package org.practice.multithreading.task5_prod_cons;
-
-import lombok.extern.slf4j.Slf4j;
+package org.example;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadLocalRandom;
 
-@Slf4j
 public class ProdCons {
+
+    private static final Integer POISON_PILL = -1;
 
     public static class Producer implements Runnable {
         BlockingQueue<Integer> queue;
         private final int maxElements;
+        private final boolean isLastProducer;
 
-        public Producer(BlockingQueue<Integer> queue, int maxElements) {
+        public Producer(BlockingQueue<Integer> queue, int maxElements, boolean isLastProducer) {
             this.queue = queue;
             this.maxElements = maxElements;
+            this.isLastProducer = isLastProducer;
         }
 
         @Override
@@ -28,10 +29,20 @@ public class ProdCons {
                 Integer value = ThreadLocalRandom.current().nextInt(100);
                 try {
                     this.queue.put(value);
+                    System.out.println("Produced by:" + Thread.currentThread().getName() + " val:" + value);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+
+            if (isLastProducer) {
+                try {
+                    queue.put(POISON_PILL);
+                    System.out.println("Producer " + Thread.currentThread().getName() + " sent POISON_PILL");
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
-                log.info("Produced by:" + Thread.currentThread().getName() + " val:" + value);
             }
         }
     }
@@ -46,24 +57,37 @@ public class ProdCons {
         @Override
         public void run() {
             while (true) {
-                if (!queue.isEmpty()) {
-                    Integer el = queue.poll();
-                    log.info("Consumed by:" + Thread.currentThread().getName() + " val:" + el);
+                try {
+                    Integer el = queue.take();
+
+                    if (el.equals(POISON_PILL)) {
+                        queue.put(POISON_PILL);
+                        System.out.println("Consumer " + Thread.currentThread().getName() + " received POISON_PILL");
+                        break;
+                    }
+
+                    System.out.println("Consumed by:" + Thread.currentThread().getName() + " val:" + el);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
                 }
             }
+            System.out.println("Consumer " + Thread.currentThread().getName() + " finished");
         }
     }
 
     public static void main(String[] args) {
-
         BlockingQueue<Integer> queue = new LinkedBlockingQueue<>(10);
+        int producerCount = 4;
 
-        for (int i = 0; i < 4; i++) {
-            new Thread(new Producer(queue, 10)).start();
+        for (int i = 0; i < producerCount; i++) {
+            boolean isLast = (i == producerCount - 1);
+            new Thread(new Producer(queue, 10, isLast), "Producer-" + i).start();
         }
 
-        for (int i = 0; i < 2; i++) {
-            new Thread(new Consumer(queue)).start();
+        int consumerCount = 2;
+        for (int i = 0; i < consumerCount; i++) {
+            new Thread(new Consumer(queue), "Consumer-" + i).start();
         }
     }
 }
